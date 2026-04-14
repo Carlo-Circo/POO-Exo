eb34f7adebe38bfe9000df9b5e539b8f9b19d473
Carlo Circo <Carlo.Circo@outlook.fr>
2026-04-14 11:55:46 +0200

    feat: annotation added to both files

--- Exo1/main.py
+++ Exo1/main.py
@@ -1,13 +1,13 @@
 from voiture import Voiture
-
+# Création de voitures en utilisant la classe Voiture.
 v1 = Voiture("Renault", "Clio", 2019, 45000, 15000)
 v2 = Voiture("Peugeot", "208", 2022, 12000, 18000)
-
+# affichage d'un kilométrage parcourue et de la valeur éstimée.
 v1.parcourir(500)
 print(v1.kilometrage)
 print(v1.estimer_valeur())
-
+# Affichage des informations de la voiture.
 v2.afficher()
-
+# Affichage du nombre de voitures créées.
 print(f' Nombre de voitures: {Voiture.nb_voitures}')
 
--- Exo1/voiture.py
+++ Exo1/voiture.py
@@ -1,6 +1,8 @@
+#Classe Voiture
 class Voiture:
+    # Initialisation de l'attribut de classe pour compter le nombre de voitures créees.
     nb_voitures = 0
-
+    # attributs: marque, modele, annee, kilometrage, prix_neuf
     def __init__(self, marque, modele, annee, kilometrage, prix_neuf):
         self.marque = marque
         self.modele = modele
@@ -8,20 +10,20 @@ class Voiture:
         self.kilometrage = kilometrage
         self.prix_neuf = prix_neuf
         Voiture.nb_voitures += 1
-
+    # Méthodes
     def afficher(self):
         print(f" Marque: {self.marque}")
         print(f" Modèle: {self.modele}")
         print(f" Année: {self.annee}")
         print(f" Kilométrage: {self.kilometrage}")
         print(f" Prix neuf: {self.prix_neuf}")
-
+    # Méthode pour vérifier si la voiture est récente
     def est_recente(self):
         return self.annee >= 2020
-    
+    # Méthode pour parcourir une distance et mettre à jour le kilomètrage.
     def parcourir(self,distance):
         self.kilometrage += distance
-
+    # Méthode pour estimer la valeur de la voiture en fonciton du kilomètrage.
     def estimer_valeur(self):
         return self.prix_neuf - (self.kilometrage * 0.05)
     
